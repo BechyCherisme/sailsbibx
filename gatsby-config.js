edcfe53a9b61779b59312e1d98ad6b2e71a8e988
bechy2018 <35705091+bechy2018@users.noreply.github.com>
2020-09-08 01:18:20 -0400

    ico -ss 99
    edit icon once again

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,46 @@ module.exports = {
   ],
 }
 
+// in gatsby-config.js
+plugins: [
+  `gatsby-plugin-favicon`
+];
+
+// in gatsby-config.js
+plugins: [
+  {
+    resolve: `gatsby-plugin-favicon`,
+    options: {
+      logo: "./src/favicon.png",
+
+      // WebApp Manifest Configuration
+      appName: null, // Inferred with your package.json
+      appDescription: null,
+      developerName: null,
+      developerURL: null,
+      dir: 'auto',
+      lang: 'en-US',
+      background: '#fff',
+      theme_color: '#fff',
+      display: 'standalone',
+      orientation: 'any',
+      start_url: '/?homescreen=1',
+      version: '1.0',
+
+      icons: {
+        android: true,
+        appleIcon: true,
+        appleStartup: true,
+        coast: false,
+        favicons: true,
+        firefox: true,
+        yandex: true,
+        windows: true,
+      }
+    }
+  }
+];
+
 {
   plugins: [`gatsby-plugin-react-helmet`]
 }
